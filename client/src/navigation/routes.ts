5ef7c92754d2349f8e82cb8f9c412cf4f561469d
medasul <medasulskyte@gmail.com>
2023-02-23 05:28:47 +0200

    route types fixed ( 2 ways provided)

--- client/src/navigation/routes.ts
+++ client/src/navigation/routes.ts
@@ -1,7 +1,37 @@
 
+
 const singleProductPageRoot = '/product/';
 
+// mes i dinaminius linkus neisime, tokie kaip single page nebus navigation'e
+// is dinaminio routo nekursime statines nuorodos
 
+// 1 budas
+
+const staticRoutes = {
+  HomePage: '/',
+  ProductFormPage: '/create-product',
+} as const;
+
+const dynamicRoutes = {
+  SingleProductPage: {
+    path: `${singleProductPageRoot}:id`,
+    createLink: (id: string | number) => `${singleProductPageRoot}${id}`,
+  },
+} as const;
+
+const routes = {
+  ...staticRoutes,
+  ...dynamicRoutes,
+} as const;
+
+export type Routes = typeof staticRoutes;
+export type RouteLink = Routes[keyof Routes];
+
+export default routes;
+
+// 2 budas
+
+/*
 const routes = {
   HomePage: '/',
   ProductFormPage: '/create-product',
@@ -10,7 +40,13 @@ const routes = {
   createLink: (id: string | number) => `${singleProductPageRoot}${id}` }
 } as const;
 
+// 2 budas 
 export type Routes = typeof routes;
-export type RouteLink = Routes[keyof Routes];
+export type RouteLink ={
+[Key in keyof Routes]: Routes[Key] extends string ? Routes[Key] : never
+} [keyof Routes]
+// export type Routes = typeof routes;
+// export type RouteLink = Routes[keyof Routes];
 
 export default routes;
+*/
